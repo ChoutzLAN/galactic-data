7f58884e1d0b1752afa1dae273ddc7975187e160
Chouta <samuel.rodda@alkemo.com>
2024-02-04 13:29:19 +1000

    0.1.1@latest-14

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
 // src/index.ts
 import { startServer } from './server/server.js'; // Assuming we move the HTTP server logic to server.ts
-
-const PORT = process.env.PORT || 3000;
-
-startServer(PORT);
+startServer();
